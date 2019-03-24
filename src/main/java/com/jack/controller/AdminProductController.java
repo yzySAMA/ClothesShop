db0e5d36304ab917afe6d63aab5349180291a477
yzySAMA <a260407834@qq.com>
2019-03-24 20:25:50 +0800

    pro_edit

--- src/main/java/com/jack/controller/AdminProductController.java
+++ src/main/java/com/jack/controller/AdminProductController.java
@@ -2,17 +2,15 @@ package com.jack.controller;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.UUID;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
-
 import com.github.pagehelper.PageInfo;
 import com.jack.common.vo.JsonResult;
+import com.jack.common.vo.UUIDUtils;
 import com.jack.entity.Product;
 import com.jack.service.ProductService;
 
@@ -22,7 +20,6 @@ public class AdminProductController {
 
 	@Autowired
 	private ProductService productService;
-
 
 	// 分页查询,模糊查询
 	@RequestMapping("doFindObject")
@@ -36,21 +33,26 @@ public class AdminProductController {
 	// 添加商品信息
 	@RequestMapping("doSaveObject")
 	@ResponseBody
-	public JsonResult doSaveObject(Product record, MultipartFile image) throws Throwable, IOException {
+	public JsonResult doSaveObject(Product record) throws Throwable, IOException {
+		productService.insertObject(record);
+		return new JsonResult("save oK");
+	}
+
+	@RequestMapping("doUploadImage")
+	@ResponseBody
+	public JsonResult doUploadImage(MultipartFile image) throws Throwable {
 		// 图片上传
 		// 设置图片名称，不能重复，可以使用uuid
-		String picName = UUID.randomUUID().toString();
+		String picName = UUIDUtils.newShortUUID();
 
 		// 获取文件名
 		String oriName = image.getOriginalFilename();
 		// 获取图片后缀
 		String extName = oriName.substring(oriName.lastIndexOf("."));
-
 		// 开始上传
-		image.transferTo(new File("/static/assets/upload" + picName + extName));
-
-		productService.insertObject(record);
-		return new JsonResult("save oK");
+		image.transferTo(new File("D:\\projects\\ClothesShop\\src\\main\\webapp\\static\\assets\\upload\\" + picName + extName));
+		String url = "static/assets/upload/"+picName + extName;
+		return new JsonResult(url);
 	}
 
 	// 根据id查询信息
